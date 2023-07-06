756ec4fd8ec51fd2aca3c46466563a42e28c784e
asters1 <gj744559140@gmail.com>
2023-07-06 12:27:28 +0800

    中断

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -17,23 +17,23 @@ require("lazy").setup({
 	
 	
 --	主题
-	-- require("config.plugins.colorscheme").background,
- --  --require("config.plugins.colorscheme").statusline1,
-	-- require("config.plugins.colorscheme").statusline2,
+	 require("config.plugins.colorscheme").background,
+   --require("config.plugins.colorscheme").statusline1,
+	 require("config.plugins.colorscheme").statusline2,
  --  --注释
-	-- require("config.plugins.comment"),
+	 require("config.plugins.comment"),
  --  --目录树
- --  require("config.plugins.nvim-tree"),
+  require("config.plugins.nvim-tree"),
  --  --git提示
- --  require("config.plugins.gitsigns"),
+   require("config.plugins.gitsigns"),
   --翻译
-  -- require("config.plugins.fy"),
+  require("config.plugins.fy"),
   --LSP
-   require("config.plugins.lspconfig"),
+  -- require("config.plugins.lspconfig"),
    
    
   --自动补全
-  require("config.plugins.autocomplete"),
+--  require("config.plugins.autocomplete"),
 
 --
 --
