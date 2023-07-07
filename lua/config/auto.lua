e19b0884508bf1b52af8c2397d8e0dcf8266aabc
asters1 <gj744559140@163.com>
2023-07-07 03:53:50 +0000

    模板与字体

--- lua/config/auto.lua
+++ lua/config/auto.lua
@@ -3,3 +3,22 @@ vim.cmd([[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe 
 --自动关闭搜索高亮
 vim.cmd([[exec "nohlsearch"]])
 
+--自动加载模板
+local temp = GetAllFile("/root/.config/nvim/lua/config/template")
+local auto_temp = [[
+    augroup python_template
+    autocmd!
+]]
+for _, f in pairs(temp) do
+  local f_type = string.match(f, "[^/]+$")
+  -- print(f_type)
+  auto_temp = auto_temp .. [[
+
+    autocmd BufNewFile *.]] .. f_type .. [[ 0r ]] .. f
+end
+auto_temp = auto_temp .. [[
+
+    augroup END
+]]
+-- print(auto_temp)
+vim.api.nvim_exec(auto_temp,true)
--- lua/util/func.lua
+++ lua/util/func.lua
@@ -1,5 +1,18 @@
+-- 遍历文件夹下的所有文件，并返回路径列表
+function GetAllFile(path)
+  local file_paths = {}                              -- 文件路径列表
+  local cmd = string.format('find %q -type f', path) -- shell 命令
+  local handle = io.popen(cmd)                       -- 执行 shell 命令并获取输出
+  local result = handle:read("*a")                   -- 读取输出
+  handle:close()
+  for file_path in result:gmatch("[^\n]+") do
+    table.insert(file_paths, file_path)
+  end
+  return file_paths
+end
+
 --判断文件或者文件夹是否存在
-IsExists=function(path)
+IsExists = function(path)
   local file = io.open(path, "rb")
   if file then
     io.close(file)
@@ -9,14 +22,12 @@ IsExists=function(path)
   end
 end
 
-IsEnableNum=function()
-
+IsEnableNum = function()
   if vim.o.number == true then
-    vim.o.relativenumber=false
+    vim.o.relativenumber = false
     vim.o.number = false
   else
     vim.o.number = true
-    vim.o.relativenumber=true
+    vim.o.relativenumber = true
   end
 end
-
