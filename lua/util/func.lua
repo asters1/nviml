9ddc77c28c7d56bac2fcce4d3cec1f6be39ba88d
asters1 <gj744559140@163.com>
2023-07-07 09:04:51 +0000

    一键运行

--- lua/util/func.lua
+++ lua/util/func.lua
@@ -31,3 +31,40 @@ IsEnableNum = function()
     vim.o.relativenumber = true
   end
 end
+
+AutoRun = function()
+  -- print("hhh")
+  --
+  vim.cmd([[
+  exec "w"
+  set splitbelow
+  :sp
+  ]])
+  local filetype = vim.bo.filetype
+  local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':t')
+  -- print(filename)
+  if IsExists("./run.sh") then
+    vim.cmd([[:term bash ./run.sh]])
+  else
+    if filetype == "go" then
+      if string.len(filename) > 8 and string.sub(filename, -8) == "_test.go" then
+        -- print(string.sub(filename, -8))
+        vim.cmd([[:term go test]])
+      else
+        vim.cmd([[:term go run .]])
+      end
+    elseif filetype == "sh" then
+      vim.cmd([[:term bash %]])
+    elseif filetype == "java" then
+      vim.cmd([[:term javac % && java %<]])
+    elseif filetype == "c" then
+      vim.cmd([[:term gcc % -o %< && ./%<]])
+    elseif filetype == "cpp" then
+      vim.cmd([[:term g++ % -o %< && ./%<]])
+    elseif filetype == "python" then
+      vim.cmd([[:term python3 %]])
+    elseif filetype == "javascript" then
+      vim.cmd([[:term node %]])
+    end
+  end
+end
