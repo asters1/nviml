d9716f6934d27e8c60beca2a17599855a731ff46
asters1 <gj744559140@gmail.com>
2023-07-08 14:44:13 +0800

    逐渐完美

--- lua/config/plugins/indent.lua
+++ lua/config/plugins/indent.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "shellRaining/hlchunk.nvim",
+ gh..   "shellRaining/hlchunk.nvim",
     init = function()
       vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { pattern = "*", command = "EnableHL", })
       require('hlchunk').setup({
