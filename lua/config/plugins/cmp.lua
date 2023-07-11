1e49312767ce39d1b83a5edb95bbb8a981107b57
asters1 <gj744559140@163.com>
2023-07-11 00:48:10 +0000

    cmp配置初步

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -109,6 +109,197 @@ return {
   }
   -- other symbols that might be useful for something: -- ⊕ † ፨ ᯾ ⁂ ∎ ∹ ☖ ⚐ 🕮 🗈 🗉 🗈 🗉 ⬠  ⬡  ⮺  ⮻ ⯐  ⯒ ⟡ ✐  ✎ ꒾꙳ ꥟ ⤙ ⤚ ⤛ ⤜
 
+ local select_opts = { behavior = cmp.SelectBehavior.Select }
+
+  cmp.setup({
+    preselect = cmp.PreselectMode.None,
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-u>"] = cmp.mapping.scroll_docs(4),
+      ["<C-l>"] = cmp.mapping.complete(),
+      ["<CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
+      ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+      ["<Down>"] = cmp.mapping.select_next_item(select_opts),
+      ["<C-n>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.jumpable(1) then
+          luasnip.jump(1)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<C-e>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      -- super tab GOOD! But I like tab to confirm
+      --["<tab>"] = cmp.mapping(function(fallback)
+      --if cmp.visible() then
+      --cmp.select_next_item()
+      --elseif luasnip.expand_or_jumpable() then
+      --luasnip.expand_or_jump()
+      --elseif has_words_before() then
+      --cmp.complete()
+      --else
+      --fallback()
+      --end
+      --end, { "i", "s" }),
+
+      --["<S-Tab>"] = cmp.mapping(function(fallback)
+      --if cmp.visible() then
+      --cmp.select_prev_item()
+      --elseif luasnip.jumpable(-1) then
+      --luasnip.jump(-1)
+      --else
+      --fallback()
+      --end
+      --end, { "i", "s" }),
+      --}),
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+        elseif require("luasnip").expand_or_jumpable() then
+          vim.fn.feedkeys(
+            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+            ""
+          )
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    }),
+    experimental = {
+      ghost_text = false,
+    },
+    window = {
+      --documentation = {
+      --border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+      --},
+      completion = {
+        border = "rounded",
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+      },
+      documentation = {
+        border = "rounded",
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+      },
+    },
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "luasnip" }, -- For luasnip users.
+      {
+        name = "buffer",
+        option = {
+          get_bufnrs = function()
+            return vim.api.nvim_list_bufs()
+          end,
+        },
+      },
+      { name = "treesitter" },
+      {
+        name = "latex_symbols",
+        option = {
+          strategy = 0, -- mixed
+        },
+      },
+      { name = "calc" },
+      { name = "path" },
+      { name = "nvim_lua" },
+      { name = "spell" },
+    }),
+    formatting = {
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        -- Kind icons
+        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+        -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+        vim_item.menu = ({
+          -- omni = "[VimTex]",
+          omni = (vim.inspect(vim_item.menu):gsub('%"', "")),
+          nvim_lsp = "[LSP]",
+          luasnip = "[Snippet]",
+          buffer = "[Buffer]",
+          spell = "[Spell]",
+          latex_symbols = "[Symbols]",
+          cmdline = "[CMD]",
+          path = "[Path]",
+        })[entry.source.name]
+        return vim_item
+      end,
+      --fields = {
+      --cmp.ItemField.Abbr,
+      --cmp.ItemField.Kind,
+      --cmp.ItemField.Menu,
+      --},
+      --format = lspkind.cmp_format({
+      --mode = "symbol_text",
+      --maxwidth = 60,
+      --before = function(entry, vim_item)
+      --vim_item.menu = ({
+      --nvim_lsp = "ﲳ",
+      --nvim_lua = "",
+      --treesitter = "",
+      --path = "ﱮ",
+      --buffer = "﬘",
+      --zsh = "",
+      --luasnip = "",
+      --spell = "",
+      --})[entry.source.name]
+
+      ---- Get the full snippet (and only keep first line)
+      --local word = entry:get_insert_text()
+      --if entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet then
+      --word = vim.lsp.util.parse_snippet(word)
+      --end
+      --word = str.oneline(word)
+      --if
+      --entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet
+      --and string.sub(vim_item.abbr, -1, -1) == "~"
+      --then
+      --word = word .. "~"
+      --end
+
+      --vim_item.abbr = word
+
+      --return vim_item
+      --end,
+      --}),
+    },
+    --enable catppuccin integration
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+      },
+    },
+  })
+
   end,
   dependencies = {
     { "hrsh7th/nvim-cmp" }, -- Autocompletion plugin
